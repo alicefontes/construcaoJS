a3d10383ff74506a8aebf7eea8eb86064d1e5123
Alice Fontes <alice.fontes@corp.globo.com>
2017-01-17 20:04:00 -0200

    refactoring

--- myscript.js
+++ myscript.js
@@ -1,48 +1,49 @@
 class Quadradinho {
-  constructor() {
+  constructor(parent) {
+    this.parent = parent
     this.elem = document.createElement('div');
-    this.elem.id = 'quadradinho';
+    this.elem.class = 'quadradinho';
     this.elem.style.position = 'absolute';
     this.elem.style.padding = '10px';
     this.elem.style.border = '1px solid white';
     this.elem.style.backgroundColor = 'white';
-    // this.elem.style.left = 10 + 'px';
-    // this.elem.style.top = 10 + 'px';
+    this.move();
+    parent.appendChild(this.elem);
+    this.boundMove = this.move.bind(this);
+    parent.addEventListener("mousemove", this.boundMove, false);
+    this.boundStopBuilding = this.stopBuilding.bind(this);
+    parent.addEventListener("click", this.boundStopBuilding);
   }
 
   move(e) {
     var e = e || window.event;
-    this.elem.style.left  = (e.clientX ) + 'px';
-    this.elem.style.top = (e.clientY ) + 'px';
+    if (e) {
+      this.elem.style.left  = (e.clientX ) + 'px';
+      this.elem.style.top = (e.clientY ) + 'px';
+    }
   }
-}
 
-function startBuilding(e) {
-  currentCursor = new Quadradinho();
-  document.body.appendChild(currentCursor.elem);
-  var callback = currentCursor.move.bind(currentCursor);
-  currentDiv.addEventListener("mousemove", callback, false);
-
-  function stopBuilding(e) {
+  stopBuilding(e) {
+    var cx = this.elem.offsetLeft;
+    var cy = this.elem.offsetTop;
     var cx2 = e.clientX;
     var cy2 = e.clientY;
-    if ( (cx == cx2-5) || (cy == cy2-5) ) {
+    console.log(cx, cy, cx2, cy2);
+    console.log(e.target, this.elem);
+    console.log(e.target != this.elem)
+    if ( e.target != this.elem && (Math.abs(cx-cx2)<=10 || Math.abs(cy-cy2)<=10) ) {
       alert("You cant add it here");
     }
     else {
-      currentDiv.removeEventListener("mousemove", callback, false);
+      this.parent.removeEventListener("mousemove", this.boundMove);
+      this.parent.removeEventListener("click", this.boundStopBuilding);
+      currentCursor = new Quadradinho(currentDiv);
     }
-    // currentDiv.removeEventListener("mousemove", callback, false);
   }
-  currentCursor.elem.addEventListener("click", stopBuilding, false);
-  var cx = e.clientX;
-  var cy = e.clientY;
 }
 
-var currentCursor = null;
 var currentDiv = document.getElementById("div1");
-
-document.getElementById("div1").addEventListener("mousemove", startBuilding, false);
+var currentCursor = new Quadradinho(currentDiv);
 
 // function startDragging() {
 //   currentCursor = new Quadradinho();
