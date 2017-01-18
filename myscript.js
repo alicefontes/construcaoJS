8033f7dd7fce23faf47c96dc54b17c2c632cf88c
Alice Fontes <alice.fontes@corp.globo.com>
2017-01-18 12:09:13 -0200

    melhorias na sintaxe

--- myscript.js
+++ myscript.js
@@ -1,11 +1,10 @@
 class Quadradinho {
   constructor(parent) {
-    this.parent = parent
+    this.parent = parent;
     this.elem = document.createElement('div');
     this.elem.class = 'quadradinho';
     this.elem.style.position = 'absolute';
     this.elem.style.padding = '10px';
-    this.elem.style.border = '1px solid white';
     this.elem.style.backgroundColor = 'white';
     this.move();
     parent.appendChild(this.elem);
@@ -18,8 +17,8 @@ class Quadradinho {
   move(e) {
     var e = e || window.event;
     if (e) {
-      this.elem.style.left  = (e.clientX ) + 'px';
-      this.elem.style.top = (e.clientY ) + 'px';
+      this.elem.style.left  = (e.clientX) + 'px';
+      this.elem.style.top = (e.clientY) + 'px';
     }
   }
 
@@ -28,10 +27,10 @@ class Quadradinho {
     var cy = this.elem.offsetTop;
     var cx2 = e.clientX;
     var cy2 = e.clientY;
-    console.log(cx, cy, cx2, cy2);
+    console.log(cx, cx2, cy, cy2);
     console.log(e.target, this.elem);
-    console.log(e.target != this.elem)
-    if ( e.target != this.elem && (Math.abs(cx-cx2)<=10 || Math.abs(cy-cy2)<=10) ) {
+    console.log(e.target != this.elem);
+    if ( (e.target != this.elem) && (Math.abs(cx-cx2)<=10 || Math.abs(cy-cy2)<=10) ) {
       alert("You cant add it here");
     }
     else {
