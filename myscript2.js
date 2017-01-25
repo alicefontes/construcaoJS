e633d5a541a5b9e5ddb82411fd35e67ac7e2b4f8
Alice Fontes <alice.fontes@corp.globo.com>
2017-01-25 18:22:31 -0200

    construção em jquery, wip

---
+++ myscript2.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+  $(document).mousemove(function(e){
+    $(".brick").css({
+       left:  e.pageX,
+       top:   e.pageY
+    });
+  });
+  $(document).click(function(e){
+    $(".wall").append("<div class='brick2' style='left: "+e.pageX+"px; top: "+e.pageY+"px;'></div>");
+  });
+});
