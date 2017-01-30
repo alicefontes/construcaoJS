518ee4abc25383c32f713b410ac68961f20e1f57
Alice Fontes <alice.fontes@corp.globo.com>
2017-01-30 15:57:18 -0200

    refactoring - adicionada classe tijolo

--- myscript2.js
+++ myscript2.js
@@ -1,13 +1,28 @@
-$(document).ready(function(){
-  $(document).mousemove(function(e){
+class Brick {
+  constructor(params) {
+    this.brick = $("<div class='brick2' style='left: " + params.left + "px; top: " + params.top + "px; width:" + params.width + "px; height:" + params.height + "px;'></div>");
+    $(".wall").append(this.brick);
+  };
+};
+
+$(document).ready(function() {
+  $(document).mousemove(function(e) {
     $(".brick").css({
        left:  e.pageX,
        top:   e.pageY
     });
   });
-  $(document).click(function(e){
-    if (event.target.id == "div1") {
-    $(".wall").append("<div class='brick2' style='left: "+e.pageX+"px; top: "+e.pageY+"px;'></div>");
+
+  $(document).click(function(e) {
+    if (event.target.id == "wall") {
+      var params = {
+        height: 38,
+        width: 80,
+        left: e.pageX,
+        top: e.pageY
+      };
+      var x = new Brick(params);
+      $(".wall").append(x);
     };
   });
 });
