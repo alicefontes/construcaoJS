a9cacef41250fa9dd8111e6a0eacc9f9945fe05d
Alice Fontes <alice.fontes@corp.globo.com>
2017-01-31 17:14:40 -0200

    mudanca de nome da classe brick2 -> fixedBrick

--- myscript2.js
+++ myscript2.js
@@ -1,21 +1,23 @@
 class Brick {
   constructor(params) {
     this.brick = $("<div></div>");
-    this.brick.css({height: params.height + "px", width: params.width + "px", left: params.left + "px", top: params.top + "px"});
+    this.brick.css({
+      height: params.height + "px",
+      width: params.width + "px",
+      left: params.left + "px",
+      top: params.top + "px"
+    });
   };
 
   stamp(currentWall) {
-    this.brick.addClass("brick2");
+    this.brick.addClass("fixedBrick");
     currentWall.append(this.brick);
   };
 };
 
 $(document).ready(function() {
   $(document).mousemove(function(e) {
-    $(".brick").css({
-       left:  e.pageX,
-       top:   e.pageY
-    });
+    $(".brick").css({ left: e.pageX, top: e.pageY });
   });
 
   $(document).click(function(e) {
@@ -26,9 +28,11 @@ $(document).ready(function() {
         left: e.pageX,
         top: e.pageY
       };
+
       var myNewBrick = new Brick(params);
       currentWall = $(".wall");
       myNewBrick.stamp(currentWall);
     };
   });
 });
+//clicar para deixar o mesmo tijolo que esta se movendo
