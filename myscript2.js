4b34b8e363e14d187dc90f1afeb9eff0f6b6338d
Alice Fontes <alice.fontes@corp.globo.com>
2017-02-02 09:55:41 -0200

    refactoring na classe tijolo -> pedreiro

--- myscript2.js
+++ myscript2.js
@@ -1,24 +1,28 @@
-class Brick {
-  constructor(params) {
-    this.brick = $("<div></div>");
-    this.brick.css({
+class BrickLayer {
+  //manter esse codigo e mudar a classe para pedreiro
+  createBrick(params) {
+    const brick = $("<div></div>")
+
+    brick.css({
       height: params.height + "px",
       width: params.width + "px",
       left: params.left + "px",
       top: params.top + "px"
-    });
-  };
+    })
 
-  stamp(currentWall) {
-    this.brick.addClass("fixedBrick");
-    currentWall.append(this.brick);
-  };
-};
+    return brick
+  }
+
+  stamp(brick, currentWall) {
+    brick.addClass("fixedBrick")
+    currentWall.append(brick)
+  }
+}
 
 $(document).ready(function() {
   $(document).mousemove(function(e) {
-    $(".brick").css({ left: e.pageX, top: e.pageY });
-  });
+    $(".brick").css({ left: e.pageX, top: e.pageY })
+  })
 
   $(document).click(function(e) {
     if (event.target.id == "wall") {
@@ -27,12 +31,12 @@ $(document).ready(function() {
         width: 80,
         left: e.pageX,
         top: e.pageY
-      };
+      }
 
-      var myNewBrick = new Brick(params);
-      currentWall = $(".wall");
-      myNewBrick.stamp(currentWall);
-    };
-  });
-});
-//clicar para deixar o mesmo tijolo que esta se movendo
+      var currentBrickLayer = new BrickLayer()
+      var brick = currentBrickLayer.createBrick(params)
+      currentWall = $(".wall")
+      currentBrickLayer.stamp(brick, currentWall)
+    }
+  })
+})
