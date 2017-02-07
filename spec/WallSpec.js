77852a52f2383144221309f48e68e505698cbd38
Alice Fontes <alice.fontes@corp.globo.com>
2017-02-07 19:25:51 -0200

    adicionado teste para parametros do tijolo

--- spec/WallSpec.js
+++ spec/WallSpec.js
@@ -1,7 +1,18 @@
 describe("TheWallTests", function() {
+  var params = { height: 38, width: 80, left: 70, top: 70 }
+  var newBrickLayer = new BrickLayer()
+  var newBrick = newBrickLayer.createBrick(params)
 
-  
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+  it("checks params of the brick created", function() {
+    expect(newBrick[0].style.height).toEqual(params.height + "px")
+    expect(newBrick[0].style.width).toEqual(params.width + "px")
+    expect(newBrick[0].style.left).toEqual(params.left + "px")
+    expect(newBrick[0].style.top).toEqual(params.top + "px")
   });
 });
+
+// expect(newBrick[0].style).toHaveCss({ height: params.height + "px",
+//       width: params.width + "px",
+//       left: params.left,
+//       top: params.top })
+// });
---
+++ src/myscript-jquery.js
@@ -0,0 +1,41 @@
+class BrickLayer {
+  createBrick(params) {
+    const brick = $("<div></div>")
+
+    brick.css({
+      height: params.height + "px",
+      width: params.width + "px",
+      left: params.left + "px",
+      top: params.top + "px"
+    })
+
+    return brick
+  }
+
+  stamp(brick, currentWall) {
+    brick.addClass("fixedBrick")
+    currentWall.append(brick)
+  }
+}
+
+$(document).ready(function() {
+  $(document).mousemove(function(e) {
+    $(".brick").css({ left: e.pageX, top: e.pageY })
+  })
+
+  $(document).click(function(e) {
+    if (event.target.id == "wall") {
+      var params = {
+        height: 38,
+        width: 80,
+        left: e.pageX,
+        top: e.pageY
+      }
+
+      var currentBrickLayer = new BrickLayer()
+      var brick = currentBrickLayer.createBrick(params)
+      currentWall = $(".wall")
+      currentBrickLayer.stamp(brick, currentWall)
+    }
+  })
+})
