af204b55833d66d81724b78d6aeabd6f419cc135
Alice Fontes <alice.fontes@corp.globo.com>
2017-02-07 19:37:34 -0200

    adicionado teste para verificar classe do tijolo apos stamp

--- spec/WallSpec.js
+++ spec/WallSpec.js
@@ -2,12 +2,18 @@ describe("TheWallTests", function() {
   var params = { height: 38, width: 80, left: 70, top: 70 }
   var newBrickLayer = new BrickLayer()
   var newBrick = newBrickLayer.createBrick(params)
+  currentWall = $(".wall")
+  newBrickLayer.stamp(newBrick, currentWall)
 
   it("checks params of the brick created", function() {
     expect(newBrick[0].style.height).toEqual(params.height + "px")
     expect(newBrick[0].style.width).toEqual(params.width + "px")
     expect(newBrick[0].style.left).toEqual(params.left + "px")
     expect(newBrick[0].style.top).toEqual(params.top + "px")
+  });
+
+  it("checks new className after stamp", function() {
+    expect(newBrick[0].className).toContain("fixedBrick")
   });
 });
 
