6ce7a034b04d0fcf08c1937f8e4db040fa6c814e
Alice Fontes <alice.fontes@corp.globo.com>
2017-02-13 15:26:41 -0200

    testa se o muro contém o tijolo após o append

--- spec/WallSpec.js
+++ spec/WallSpec.js
@@ -1,12 +1,20 @@
 describe("TheWallTests", function() {
 
   describe("when created a new brick", function() {
-    var params = { height: 38, width: 80, left: 70, top: 70 }
-    var newBrickLayer = new BrickLayer()
-    var newBrick = newBrickLayer.createBrick(params)
+    var params
+    var newBrickLayer
+    var newBrick
     setFixtures('<div class="wall" id="wall"></div>')
-    var currentWall = $(".wall")
-    newBrickLayer.stamp(newBrick, currentWall)
+    var currentWall
+
+    beforeEach(function() {
+      params = { height: 38, width: 80, left: 70, top: 70 }
+      newBrickLayer = new BrickLayer()
+      newBrick = newBrickLayer.createBrick(params)
+      setFixtures('<div class="wall" id="wall"></div>')
+      currentWall = $(".wall")
+      newBrickLayer.stamp(newBrick, currentWall)
+    });
 
     it("checks params of the brick created", function() {
       expect(newBrick[0].style.height).toEqual(params.height + "px")
@@ -19,12 +27,10 @@ describe("TheWallTests", function() {
       expect(newBrick[0].className).toContain("fixedBrick")
     });
 
-    // it("checks if currentWall contains newBrick", function() {
-    //   setFixtures('<div class="wall" id="wall"></div>')
-    //   var currentWall = $(".wall")
-    //   expect(currentWall).toContain(newBrick)
-    // });
-  })
+    it("checks if currentWall contains newBrick", function() {
+      expect(currentWall[0].contains(newBrick[0])).toBe(true)
+    });
+  });
 
   describe('when called onReady', function() {
     beforeEach(function() {
@@ -44,4 +50,35 @@ describe("TheWallTests", function() {
       expect(BrickLayer.prototype.stamp).not.toHaveBeenCalled()
     });
   });
+
+//nao funciona desse jeito pq criei na mão
+//tem que fazer um evento de clique em cima do tijolo já existente
+  // describe('another brick already stamped', function() {
+  //   var anotherBrick
+  //
+  //   beforeEach(function() {
+  //     onReady()
+  //
+  //     var params = { height: 38, width: 80, left: 70, top: 70 }
+  //     var newBrickLayer = new BrickLayer()
+  //     var newBrick = newBrickLayer.createBrick(params)
+  //     setFixtures('<div class="wall" id="wall"></div>')
+  //     var currentWall = $(".wall")
+  //     newBrickLayer.stamp(newBrick, currentWall)
+  //
+  //     var params2 = { height: 38, width: 80 }
+  //
+  //     // document.elementFromPoint(x, y).click();
+  //     var e = new jQuery.Event("click");
+  //     e.pageX = 172;
+  //     e.pageY = 172;
+  //     var anotherBrickLayer = new BrickLayer()
+  //     anotherBrick = anotherBrickLayer.createBrick(params2)
+  //     $("#wall").trigger(e);
+  //   });
+  //
+  //   // it("checks if a new brick is not created when clicking on another brick", function() {
+  //   //   expect(anotherBrick[0].className).not.toContain("fixedBrick")
+  //   // });
+  // });
 });
