080eb5e239e7db2a5b192c39735995a619c4b559
Alice Fontes <alice.fontes@corp.globo.com>
2017-02-18 16:25:52 -0200

    refactoring nos testes

--- spec/WallSpec.js
+++ spec/WallSpec.js
@@ -15,50 +15,52 @@ describe("TheWallTests", function() {
       newBrickLayer.stamp(newBrick, currentWall)
     });
 
-    it("checks params of the brick created", function() {
-      expect(newBrick[0].style.height).toEqual(params.height + "px")
-      expect(newBrick[0].style.width).toEqual(params.width + "px")
-      expect(newBrick[0].style.left).toEqual(params.left + "px")
-      expect(newBrick[0].style.top).toEqual(params.top + "px")
+    describe("first checks", function(){
+      it("checks params of the brick created", function() {
+        expect(newBrick[0].style.height).toEqual(params.height + "px")
+        expect(newBrick[0].style.width).toEqual(params.width + "px")
+        expect(newBrick[0].style.left).toEqual(params.left + "px")
+        expect(newBrick[0].style.top).toEqual(params.top + "px")
+      });
+
+      it("checks new className after stamp", function() {
+        expect(newBrick[0].className).toContain("fixedBrick")
+      });
+
+      it("checks if currentWall contains newBrick", function() {
+        expect(currentWall[0].contains(newBrick[0])).toBe(true)
+      });
     });
 
-    it("checks new className after stamp", function() {
-      expect(newBrick[0].className).toContain("fixedBrick")
+    describe("when clicking on another brick", function() {
+      it("checks if a new brick is not created", function() {
+        spyOn(BrickLayer.prototype, 'stamp')
+        // BrickLayer.prototype.stamp = jasmine.createSpy()
+        newBrick.click();
+        expect(BrickLayer.prototype.stamp).not.toHaveBeenCalled()
+      });
+
+      it("checks if a new brick is created", function() {
+        spyOn(BrickLayer.prototype, 'stamp')
+        $("#wall").click();
+        expect(BrickLayer.prototype.stamp).toHaveBeenCalled()
+      });
     });
 
-    it("checks if currentWall contains newBrick", function() {
-      expect(currentWall[0].contains(newBrick[0])).toBe(true)
+    describe("when clicking on the wall", function() {
+      it("checks if a new brick is created", function() {
+        spyOn(BrickLayer.prototype, 'stamp')
+        $("#wall").click()
+        expect(BrickLayer.prototype.stamp).toHaveBeenCalled()
+      });
     });
 
-    it("checks if a new brick is not created when clicking on another brick", function() {
-      spyOn(BrickLayer.prototype, 'stamp')
-      // BrickLayer.prototype.stamp = jasmine.createSpy()
-      newBrick.click();
-      expect(BrickLayer.prototype.stamp).not.toHaveBeenCalled()
-    });
-
-    it("checks if a new brick is created when not clicking on another brick", function() {
-      spyOn(BrickLayer.prototype, 'stamp')
-      $("#wall").click();
-      expect(BrickLayer.prototype.stamp).toHaveBeenCalled()
-    });
-
-    it("checks if a new brick is created when clicking on the wall", function() {
-      spyOn(BrickLayer.prototype, 'stamp')
-      $("#wall").click()
-      expect(BrickLayer.prototype.stamp).toHaveBeenCalled()
-    });
-
-    it("checks if a new brick is not created when clicking off the wall", function() {
-      spyOn(BrickLayer.prototype, 'stamp')
-      $("body").click()
-      expect(BrickLayer.prototype.stamp).not.toHaveBeenCalled()
+    describe("when clicking off the wall", function() {
+      it("checks if a new brick is not created", function() {
+        spyOn(BrickLayer.prototype, 'stamp')
+        $("body").click()
+        expect(BrickLayer.prototype.stamp).not.toHaveBeenCalled()
+      });
     });
   });
-  // describe("when called onReady", function() {
-  //   beforeEach(function() {
-  //     setFixtures('<div id="wall" class="wall"></div>')
-  //     onReady()
-  //   });
-  // });
 });
