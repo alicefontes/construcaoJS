cb6624dfb231dd480645a264997d5d127ec4e8be
Alice Fontes <alice.fontes@corp.globo.com>
2017-02-20 19:05:46 -0300

    clientX->pageX, fixar tijolo no clique-wip

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,23 +8,24 @@ var Brick = require('./Brick.jsx');
 
 class Wall extends Component {
   constructor(props) {
-    super(props);
-    this.state = {moveX: "", moveY: "", fixX: "", fixY: ""};
+    super(props)
+    this.state = {moveX: "", moveY: "", fixX: "", fixY: "", isClicked: false}
     //define aqui e coloca o bind p poder usar o this.nome nas funcoes
-    this.handleClick = this.handleClick.bind(this);
-    this.move = this.move.bind(this);
+    this.handleClick = this.handleClick.bind(this)
+    this.move = this.move.bind(this)
     document.addEventListener("mousemove", this)
   }
 
   handleClick(e) {
     console.log("clicou")
     console.log(e.clientX, e.clientY)
-    this.setState({fixX: e.clientX, fixY: e.clientY});
+    this.setState({isClicked: true, fixX: e.clientX, fixY: e.clientY})
+    // e.stopPropagation()
   }
 
   move(e) {
     console.log(e.clientX, e.clientY)
-    this.setState({moveX: e.clientX, moveY: e.clientY});
+    this.setState({isClicked: false, moveX: e.pageX, moveY: e.pageY})
   }
 
   render() {
@@ -39,7 +40,7 @@ class Wall extends Component {
         </p>
         <div className="Wall" onClick={this.handleClick} onMouseMove={this.move} >
           <img src={pink} className="pink"/>
-          <Brick positionX={this.state.moveX} positionY={this.state.moveY} setPositionX={this.state.fixX} setPositionY={this.state.fixY}>
+          <Brick clicked={this.state.isClicked} positionX={this.state.moveX} positionY={this.state.moveY} setPositionX={this.state.fixX} setPositionY={this.state.fixY}>
           </Brick>
         </div>
       </div>
--- my-app/src/Brick.jsx
+++ my-app/src/Brick.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 
 class Brick extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
-    
-      var divStyle = {
-        color: 'white',
-        top: this.props.positionY,
-        left: this.props.positionX
-      };
+    var fixedBrick = this.props.isClicked ? (new Brick(), console.log("novo")) : false
+
+    var divStyle = {
+      color: 'white',
+      top: this.props.positionY,
+      left: this.props.positionX
+    };
 
     return (
       <div className="Brick" style={divStyle}>
