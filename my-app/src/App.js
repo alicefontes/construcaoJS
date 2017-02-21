fe60efe82a2fd8f85f9041cd9378a578c0d13bdd
Alice Fontes <alice.fontes@corp.globo.com>
2017-02-21 11:09:42 -0300

    refactoring e exclusao dos ;

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import pink from './pf.jpg';
-import './App.css';
-import './Wall.css';
-
-var Brick = require('./Brick.jsx');
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import pink from './pf.jpg'
+import './App.css'
+import './Wall.css'
+import Brick from './Brick.jsx'
 
 class Wall extends Component {
   constructor(props) {
     super(props)
     this.state = {moveX: "", moveY: "", fixX: "", fixY: "", isClicked: false}
-    //define aqui e coloca o bind p poder usar o this.nome nas funcoes
-    this.handleClick = this.handleClick.bind(this)
-    this.move = this.move.bind(this)
     document.addEventListener("mousemove", this)
   }
 
-  handleClick(e) {
+  handleClick = (e) => {
     console.log("clicou")
     console.log(e.clientX, e.clientY)
-    this.setState({isClicked: true, fixX: e.clientX, fixY: e.clientY})
-    // e.stopPropagation()
+    this.setState({ isClicked: true, fixX: e.pageX, fixY: e.pageY })
   }
 
-  move(e) {
+  move = (e) => {
     console.log(e.clientX, e.clientY)
-    this.setState({isClicked: false, moveX: e.pageX, moveY: e.pageY})
+    this.setState({ isClicked: false, moveX: e.pageX, moveY: e.pageY })
   }
 
   render() {
@@ -38,15 +33,18 @@ class Wall extends Component {
         <p className="App-intro">
           Click anywhere to start building.
         </p>
-        <div className="Wall" onClick={this.handleClick} onMouseMove={this.move} >
-          <img src={pink} className="pink"/>
-          <Brick clicked={this.state.isClicked} positionX={this.state.moveX} positionY={this.state.moveY} setPositionX={this.state.fixX} setPositionY={this.state.fixY}>
-          </Brick>
+        <div className="Wall" onClick={this.handleClick} onMouseMove={this.move}>
+          <img src={pink} className="pink" />
+          <Brick
+            clicked={this.state.isClicked}
+            positionX={this.state.moveX}
+            positionY={this.state.moveY}
+            setPositionX={this.state.fixX}
+            setPositionY={this.state.fixY} />
         </div>
       </div>
-    );
+    )
   }
 }
 
-
-export default Wall;
+export default Wall
--- my-app/src/Brick.jsx
+++ my-app/src/Brick.jsx
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
 
 class Brick extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
   render() {
-    var fixedBrick = this.props.isClicked ? (new Brick(), console.log("novo")) : false
+    // const fixedBrick = this.props.isClicked ? (new Brick(), console.log("novo")) : false
 
-    var divStyle = {
-      color: 'white',
-      top: this.props.positionY,
-      left: this.props.positionX
-    };
+    const { positionX, positionY } = this.props
+
+    const divStyle = {
+      top: positionY,
+      left: positionX
+    }
 
     return (
-      <div className="Brick" style={divStyle}>
-      </div>
-    );
+      <div className="Brick" style={divStyle} />
+    )
   }
-
 }
 
-module.exports = Brick;
+module.exports = Brick
