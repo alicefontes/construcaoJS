e1ef4f0ca8862e0e38f88c675f599f302bcb9d41
Alice Fontes <alice.fontes@corp.globo.com>
2017-03-06 20:28:26 -0300

    verifica se o tijolo foi criado

--- wall_react/src/App.test.js
+++ wall_react/src/App.test.js
@@ -37,14 +37,14 @@ it('type of brick', () => {
   expect(resultBrick.type).toBe('div');
 })
 
-it('should render three <Foo /> components', () => {
+it('', () => {
   const wrapper = shallow(<Wall />);
   expect(wrapper.find(Wall))
 });
 
 
-describe('<Wall />', () => {
-  it('simulates click events', () => {
+describe('when clicking on the wall', () => {
+  it('calls handleClick', () => {
     const wrapper = mount(<Wall />);
     const instance = wrapper.instance();
 
@@ -54,5 +54,28 @@ describe('<Wall />', () => {
     wrapper.find('.Wall').simulate('click')
 
     expect(instance.handleClick).toHaveBeenCalled()
+  });
+
+  it('fix the brick', () => {
+    const wrapper = mount(<Wall />);
+    const instance = wrapper.instance();
+
+    spyOn(instance, 'state')
+    wrapper.update()
+
+    const wall = wrapper.find('.Wall')
+    const event = {
+      target: {
+        id: 'wall'
+      },
+      pageX: 277+"px",
+      pageY: 288+"px"
+    }
+
+    wall.props().onClick(event)
+
+    expect(instance.state.isClicked).toEqual(1)
+    expect(instance.state.fixX).toEqual("277px")
+    expect(instance.state.fixY).toEqual("288px")
   });
 })
